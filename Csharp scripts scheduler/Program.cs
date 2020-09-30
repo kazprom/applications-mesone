713661bde39418b286addcc39c5bef0c1417a573
KorolIgr <korol.igor.alexandrovi4@gmail.com>
2020-09-30 16:21:09 +0600

    Инициализировать проекты

---
+++ Csharp scripts scheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_scripts_scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ OPC DB gate client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OPC_DB_gate_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
