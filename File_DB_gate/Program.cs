5c7ffbc168b305e7eb621c69a4b7ad149700d601
KorolIgr <korol.igor.alexandrovi4@gmail.com>
2020-10-21 12:41:26 +0600

    Слияние веток в master + добавился проект File_DB_gate

---
+++ File_DB_gate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace File_DB_gate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
