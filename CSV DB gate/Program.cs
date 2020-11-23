448a567320236ceae2bc6cc697e61a301d3b253d
KorolIgr <korol.igor.alexandrovi4@gmail.com>
2020-11-23 20:15:21 +0600

    Добавить функционал CSV DB gate может пока читать файлы с помощью библиотеки CSVhelper

--- CSV DB gate/Program.cs
+++ CSV DB gate/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -18,19 +22,100 @@ namespace CSV_DB_gate
 {
     class Program
     {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main()
         {
-            Lib.Common.PrintAppInfo();
-            Lib.Common.Subscribe_Ctrl_C();
+            try
+            {
+                Lib.Common common = new Lib.Common();
 
-            Lib.Console console = new Lib.Console();
-            LibMESone.Loggers.TextLogger text_logger = new LibMESone.Loggers.TextLogger();
+                string value;
+                using (var reader = new StreamReader(@"c:\temp\CSVtest\AB0000007.csv"))
+                using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.CurrentCulture))
+                {
+                    Console.WriteLine(csv.Configuration.Delimiter);
+                    Console.WriteLine(csv.Configuration.Quote);
+                    Console.WriteLine(csv.Configuration.DetectColumnCountChanges);
+                    Console.WriteLine(csv.Configuration.HasHeaderRecord);
+                    csv.Configuration.IgnoreQuotes = true;
 
-            LibMESone.ConfigFile config_file = new LibMESone.ConfigFile();
+                    csv.Read();
+                    csv.ReadHeader();
+                    string[] headerRow = csv.Context.HeaderRecord;
+
+                    csv.Configuration.PrepareHeaderForMatch = (header, index) =>
+                    {
+                        if (string.IsNullOrWhiteSpace(header))
+                        {
+                            return $"Blank{index}";
+                        }
+
+                        return header;
+                    };
+
+                    var records = csv.GetRecords<dynamic>().ToList();
+
+                    Console.ReadKey();
+
+
+                    /*
+                    //csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
+
+                    foreach (var item in headerRow)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    var records = csv.GetRecords<dynamic>() as IDictionary<string, object>;
+
+                    foreach (var item in records)
+                    {
+                        Console.WriteLine(item.GetType());
+                    }
+
+                    */
+
+                    //csv.GetRecords<string>();
+
+                    //csv.ReadHeader();
+
+
+                    /*
+                    csv.Configuration.BadDataFound = x =>
+                    {
+
+                        Console.WriteLine($"Bad data: <{x.RawRecord}>");
+                        Console.ReadKey();
+                    };
+
+                    for (int i = 1; csv.Read(); i++)
+                    {
+                        Console.Write(i + " ");
+                        for (int j = 1; csv.TryGetField<string>(j, out value); j++)
+                        {
+                            Console.Write(value + " | ");
+                        }
+                        Console.WriteLine("");
+                    }
+                    */
+                }
 
 
 
-            Lib.Common.InfinityWaiting();
+
+
+                common.InfinityWaiting();
+
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex);
+            }
         }
+
+
+
+
     }
 }
---
+++ MELSEC DB gate/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace MELSEC_DB_gate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                TcpClient client = new TcpClient("192.168.0.252", 5000);
+
+                String cmd = "";
+                String OutAddress = "0001";
+                cmd = "";
+                cmd = cmd + "5000";// sub HEAD (NOT)
+                cmd = cmd + "00";//   network number (NOT)
+                cmd = cmd + "FF";//PLC NUMBER
+                cmd = cmd + "03FF";// DEMAND OBJECT MUDULE I/O NUMBER
+                cmd = cmd + "00";//  DEMAND OBJECT MUDULE DEVICE NUMBER
+                cmd = cmd + "001C";//  Length of demand data
+                cmd = cmd + "000A";//  CPU inspector data
+                cmd = cmd + "0401";//  Read command
+                cmd = cmd + "0000";//  Sub command
+                cmd = cmd + "D*";//   device code
+                cmd = cmd + "009500"; //adBase 
+                cmd = cmd + OutAddress;  //BASE ADDRESS  
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(cmd);
+
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", cmd);
+
+                data = new Byte[256];
+
+                String responseData = String.Empty;
+
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
